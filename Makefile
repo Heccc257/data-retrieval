f5e310d1b261d258376053c1b08d82d1df10c5d2
hch <2762312941@qq.com>
2022-12-05 11:27:35 +0800

    修改了README 删除了makefile 运行后生成handin

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	g++ --shared scheduler.cpp anneal.cpp greedy.cpp scheduler.h -o myscheduler.so -fPIC -O3
-
-clean:
-	rm myscheduler.so
--- test.py
+++ test.py
@@ -121,8 +121,13 @@ if __name__ == '__main__':
     for s in schedulers:
         os.system(f'g++ --shared src/{s}.cpp {appendSrc} -o build/my{s}.so -fPIC -O3')
     scores={s:{} for s in schedulers}
-        
-
+    
+    # make handin
+    if os.path.exists('handin') == False:
+        os.makedirs('handin',exist_ok=True)
+    handinList = " Scheduler.py please\ README\ first.md build "
+    os.system(f'cp -rf {handinList} handin')
+    os.system('touch handin/请一定看看\ please\ REDAME\ first.md!!!')
     
     data=[]
     if(cfg.data_dir=='data/'):
